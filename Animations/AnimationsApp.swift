48e3c2b33c4a6b1eac1a5bd9b68c62d051e0f896
Woodrow Martyr <woodrow.martyr@gmail.com>
2024-01-10 11:16:28 +1030

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Woodrow Martyr on 10/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
